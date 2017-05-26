86b39cb4202f41deaeded264a9d0c83cff55d0c8
Gökhan Demir <mail@gdemir.me>
2017-05-26 22:42:46 +0300

    Update backtotop.js

--- backtotop.js
+++ backtotop.js
@@ -1,19 +1,28 @@
-if ($('#backtotop').length) {
-  var scrollTrigger = 100, // px
-  backToTop = function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > scrollTrigger) {
-      $('#backtotop').addClass('show');
-    } else {
-      $('#backtotop').removeClass('show');
-    }
-  };
-  backToTop();
-  $(window).bind('scroll', function () {
+(function($) {
+
+  if ($('#backtotop').length) {
+
+    var scrollTrigger = 100, // px
+    backToTop = function () {
+      var scrollTop = $(window).scrollTop();
+      if (scrollTop > scrollTrigger) {
+        $('#backtotop').addClass('show');
+      } else {
+        $('#backtotop').removeClass('show');
+      }
+    };
+
     backToTop();
-  });
-  $('#backtotop').on('click', function (e) {
-    e.preventDefault();
+
+    $(window).bind('scroll', function () {
+      backToTop();
+    });
+
+  }
+
+  $('#backtotop').click( function (event) {
+    event.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, 800);
   });
-}
+
+})(jQuery);
